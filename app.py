8eb587aaca2867d8b1975e73cadb7bce6f5b0173
jdiaz <jayremie.diaz@gmail.com>
2024-11-10 09:14:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ warnings.filterwarnings("ignore")
 st.set_page_config(page_title="The Shakespeare Bot: Ask William Anything!", page_icon="🎭", layout="wide")
 
 with st.sidebar:
-    st.image('images/logo1.png')
-    st.image('images/logo0.png')
+#    st.image('images/logo1.png')
+#    st.image('images/logo0.png')
     
     openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
     if not (openai.api_key.startswith('sk-') and len(openai.api_key) == 164):
